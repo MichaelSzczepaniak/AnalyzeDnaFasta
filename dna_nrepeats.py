47b07e72bc90d3903f0f323bf311638e9820be16
MichaelSzczepaniak <mszczepaniak@pobox.com>
2016-07-26 13:49:25 -0600

    dna_nrepeats.py - rewrite getNRepeats function so that values in returned dictionary are either counts or lists of position index values

--- dna_nrepeats.py
+++ dna_nrepeats.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys, read_fasta as rfa
 
-def getNRepeatCounts(dna, n=2) :
+def getNRepeats(dna, n=2, values_as_locations=True) :
     """ Returns a dictionary where the keys are the substrings
-    of length n in dna and values are the frequency/count of each
-    substring found in dna.
+    of length n in dna and values are are either 1) the index of each
+    substring found in dna if values_as_locations = True or
+    2) the frequency/count of each substring if values_as_locations = False.
     
     dna - string, rep'n of DNA sequence of A's, T's, G's and C's which
           can be upper or lower case
@@ -16,37 +17,18 @@ def getNRepeatCounts(dna, n=2) :
     repeats = {}
     for i in range(0, len(dna)-n) :
         substr = dna[i:i+n]
-        repeats[substr] = 0
+        if values_as_locations :
+            repeats[substr] = []
+        else :
+            repeats[substr] = 0
     # Count the instances of each keys
     for key in repeats.keys() :
         for j in range(0, len(dna)-n) :
             subdna = dna[j:j+n]
             if key == subdna :
-                repeats[key] += 1
-                
-    return repeats
-    
-def getNRepeatPositions(dna, n=2) :
-    """ Returns a dictionary where the keys are the substrings
-    of length n in dna and values are the index of each substring
-    found in dna.
-    
-    dna - string, rep'n of DNA sequence of A's, T's, G's and C's which
-          can be upper or lower case
-    n - integer, allowable value: 1 to len(dna), length of substrings
-        to count frequencies on
-    """
-    dna = dna.lower()
-    # Get all the substrings and set them as keys in our dictionary
-    repeats = {}
-    for i in range(0, len(dna)-n) :
-        substr = dna[i:i+n]
-        repeats[substr] = []
-    # Count the instances of each keys
-    for key in repeats.keys() :
-        for j in range(0, len(dna)-n) :
-            subdna = dna[j:j+n]
-            if key == subdna :
-                repeats[key].append(j)
+                if values_as_locations :
+                    repeats[key].append(j)
+                else :
+                    repeats[key] += 1
                 
     return repeats
