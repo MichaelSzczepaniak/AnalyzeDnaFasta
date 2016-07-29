c229d34db5df2cfb2d5787fa150d86fd33f1673f
MichaelSzczepaniak <mszczepaniak@pobox.com>
2016-07-29 16:25:25 -0600

    dna_nrepeats.py - change values_as_locations parameter to values_are_counts, update logic and comments to reflect changes in getNRepeats function, impl getMostFrequentRepeatN function fix call to getOccsOfRepeatInSingleSeq function and fix comments in getOccsOfRepeatInAllSeqs

--- dna_nrepeats.py
+++ dna_nrepeats.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys, read_fasta as rfa
 
-def getNRepeats(dna, n=2, values_as_locations=True) :
+
+def getNRepeats(dna, n=2, values_are_counts=True) :
     """ Returns a dictionary where the keys are the substrings
     of length n in dna and values are are either 1) the index of each
-    substring found in dna if values_as_locations = True or
-    2) the frequency/count of each substring if values_as_locations = False.
+    substring found in dna if values_are_counts = False or
+    2) the frequency/count of each substring if values_are_counts = True
+    (default).
     
     dna - string, rep'n of DNA sequence of A's, T's, G's and C's which
           can be upper or lower case
@@ -17,49 +18,71 @@ def getNRepeats(dna, n=2, values_as_locations=True) :
     repeats = {}
     for i in range(0, len(dna)-n) :
         substr = dna[i:i+n]
-        if values_as_locations :
-            repeats[substr] = []
-        else :
+        if values_are_counts :
             repeats[substr] = 0
+        else :
+            repeats[substr] = []
     # Count the instances of each keys
     for key in repeats.keys() :
         for j in range(0, len(dna)-n) :
             subdna = dna[j:j+n]
             if key == subdna :
-                if values_as_locations :
-                    repeats[key].append(j+1) # convert to 1-based indices
-                else :
+                if values_are_counts :
                     repeats[key] += 1
+                else :
+                    repeats[key].append(j+1) # convert to 1-based indices
                 
     return repeats
     
-def getOccsOfRepeatInSingleSeq(dna_seq, repeat) :
-    """ Returns an integer which is the number of occurance of repeat
-    in dna_seq
+def getMostFrequentRepeatN(dna_seqs, nrep) :
+    """ Returns a string that is the most frequently occuring repeat
+    of size nrep.
+    
+    dna_seqs - Dictionary where keys = sequence identifiers and values =
+    DNA seqeunce for that record.
+    nrep - Size of the repeat to search on.
+    
     """
-    all_repeats = getNRepeats(dna, len(repeat), False)
+    longest_n_repeat = ""
+    longest_n_sub_count = -1
+    for seq_id in dna_seqs.keys() :
+        dna = dna_seqs[seq_id]
+        repeats = getNRepeats(dna, nrep)
+        for nrepeat, nrep_count in repeats.items() :
+            if nrep_count > longest_n_sub_count :
+                longest_n_sub_count = nrep_count
+                longest_n_repeat = nrepeat
+    
+    return longest_n_repeat
+    
+    
+def getOccsOfRepeatInSingleSeq(dna, repeat) :
+    """ Returns an integer which is the number of occurance of repeat
+    in dna
+    """
+    all_repeats = getNRepeats(dna, len(repeat))
     occurances = all_repeats.get(repeat, 0)
     
     return occurances
     
 def getOccsOfRepeatInAllSeqs(dna_seqs, repeat) :
-    """ Returns a dictionary with keys that are the keys of dna_seqs and
+    """ Returns a dictionary with keys that are sequence ids and
     values that are integers which are the number of occurance of repeat
     in each of the sequences in dna_seqs
     """
     result = {}
     for sid, dna in dna_seqs.items() :
-        result[sid] = getMaxOccOfRepeatInSingleSeq(dna, repeat)
+        result[sid] = getOccsOfRepeatInSingleSeq(dna, repeat)
         
     return result
     
 def getAllOccsOfRepeat(dna_seqs, repeat) :
     """ Returns an integer that's the count of all instance of repeat
-    in each sequence of dna_seqs.
+    in each sequence in dna_seqs.
     """
     result = 0
-    repeats_in_all_seqs = getOccsOfRepeatInAllSeqs(dna_seqs, repeat)
-    for count in repeats_in_all_seqs.values() :
+    getAllOccsOfRepeat = getOccsOfRepeatInAllSeqs(dna_seqs, repeat)
+    for count in getAllOccsOfRepeat.values() :
         result += count
     
     return result
